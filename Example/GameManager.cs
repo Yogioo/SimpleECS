029a495122eb6838ea949b5e2a07dc5fed09d61c
Yogi <1273750265@qq.com>
2023-01-10 15:38:49 +0800

    Add Example Script

---
+++ Example/GameManager.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SimpleECS.Example
+{
+    public class GameManager : MonoBehaviour
+    {
+        private Entity player;
+        private List<Entity> enemies;
+        private World mainWorld;
+        private void Start()
+        {
+            mainWorld = World.Create("Main");
+
+            player = mainWorld.CreateEntity("Player",
+                new PositionComponent(Vector3.zero),
+                new RotationComponent(Quaternion.identity));
+            
+            enemies = new List<Entity>();
+            for (int i = 0; i < 1000; i++)
+            {
+                enemies.Add(
+                mainWorld.CreateEntity($"Enemy",
+                    new PositionComponent(Vector3.forward * i),
+                    new RotationComponent(Quaternion.Euler(new Vector3(0,180,0)))));
+            }
+
+            player.Set(new InputComponent(Vector2.zero));
+            // enemyOne.Set(new InputComponent(Vector2.zero));
+            // enemyTwo.Set(new InputComponent(Vector2.zero));
+        }
+
+        private void Update()
+        {
+            // Same like Singleton System, Only One
+            var x = Input.GetAxis("Horizontal");
+            var y = Input.GetAxis("Vertical");
+            {
+                ref var inputComponent = ref player.Get<InputComponent>();
+                inputComponent.Value = new Vector2(x, y);
+            }
+
+            if (Input.GetKey(KeyCode.Return))
+            {
+                enemies.Add(
+                    mainWorld.CreateEntity($"Enemy",
+                        new PositionComponent(Vector3.forward),
+                        new RotationComponent(Quaternion.Euler(new Vector3(0,180,0)))));
+            }
+            
+            if (Input.GetKey(KeyCode.Space))
+            {
+                enemies[enemies.Count-1].Destroy();
+                enemies.RemoveAt(enemies.Count-1);
+            }
+        }
+    }
+
+    public struct PositionComponent
+    {
+        public Vector3 position;
+
+        public PositionComponent(Vector3 position)
+        {
+            this.position = position;
+        }
+
+        public override string ToString()
+        {
+            return position.ToString();
+        }
+    }
+
+    public struct RotationComponent
+    {
+        public Quaternion rotation;
+
+        public RotationComponent(Quaternion rotation)
+        {
+            this.rotation = rotation;
+        }
+        public override string ToString()
+        {
+            return rotation.ToString();
+        }
+    }
+
+    public struct InputComponent
+    {
+        public Vector2 Value;
+
+        public InputComponent(Vector2 value)
+        {
+            Value = value;
+        }
+        
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
---
+++ SimpleECS/Editor/UltimateECSDebugWindow.cs
@@ -0,0 +1,67 @@
+﻿using SimpleECS.Internal;
+using UnityEditor;
+using UnityEngine;
+
+namespace SimpleECS.Example.Editor
+{
+    public class UltimateECSDebugWindow : EditorWindow
+    {
+        [MenuItem("Window/UltimateECS/Debug")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<UltimateECSDebugWindow>();
+            window.titleContent = new GUIContent("Ultimate ECS Debug");
+            window.Show();
+        }
+
+        private Vector2 pos;
+
+        private void OnGUI()
+        {
+            if (!Application.isPlaying)
+            {
+                GUILayout.Label("Game Paused~");   
+                return;
+            }
+            World[] worlds = World.GetAll();
+            foreach (var world in worlds)
+            {
+                GUILayout.Label($"Entity Free:{Entities.Free.Count}");
+                pos = EditorGUILayout.BeginScrollView(pos);
+                GUILayout.Label($"World:{world.Name},Entity Count:{world.EntityCount}");
+                var entities = world.GetEntities();
+                foreach (var entity in entities)
+                {
+                    GUILayout.Box($"--------{entity.Get<string>()}:{entity.index}----------");
+
+                    ref var info = ref Entities.All[entity.index];
+                    var componentCount = info.arch_info.component_count;
+                    ref var componentBuffers = ref info.arch_info.component_buffers;
+                    var infoArchIndex = info.arch_index;
+
+                    for (int i = 0; i < componentCount; i++)
+                    {
+                        var component = componentBuffers[i].buffer.array[infoArchIndex];
+                        if (component is string)
+                        {
+                            continue;
+                        }
+
+                        if (component != null)
+                        {
+                            GUILayout.Label($"{component.GetType().Name}:{component}");
+                        }
+                        else
+                        {
+                            GUILayout.Label($"NULL");
+                        }
+                    }
+                }
+
+                EditorGUILayout.EndScrollView();
+            }
+
+            this.Repaint();
+        }
+    }
+}
